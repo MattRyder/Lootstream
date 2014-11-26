a017e6f5fd8ad0563da298a560545f4fbc1a001d
Matt Ryder <matt@mattryder.co.uk>
2014-11-26 19:35:43 +0000

    Redirect to channels if logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,13 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:code]
+    if user_signed_in?
+      redirect_to channels_path 
+    elsif params[:code]
       data = twitch.auth(params[:code])
       session[:access_token] = data[:body]["access_token"]
       user_data = twitch_auth(session[:access_token]).getYourUser
       @email = user_data[:body]['email']
     end
-    render layout: 'homepage'
   end
 
   def contact
